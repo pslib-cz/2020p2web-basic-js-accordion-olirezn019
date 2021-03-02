fb363d8baf8fc9228ffa8fcbf1c9bf18e43dfa0a
Oliver Řezníček <u19.423@pslib.cloud>
2021-03-02 22:38:22 +0100

    complete program

---
+++ hamburger_script.js
@@ -0,0 +1,19 @@
+var icons = document.querySelectorAll(".plus-detail-checkbox");
+var boxes = document.getElementsByClassName("detail-vypujcky-container");
+
+// plus-detail-checkbox detail-vypujcky-container
+//var icons = document.getElementsByClassName("plus-detail-checkbox");
+//var boxes = document.getElementsByClassName("detail-vypujcky-container");
+
+for (let i = 0; i < icons.length; i++) {
+    let input = icons[i].querySelector("input");
+    input.onclick = () => {
+        if (input.checked) {;
+            boxes[i].style.display = "block";
+            boxes[i].style.height = boxes[i].scrollHeight+"px";
+        }
+        else{
+            boxes[i].style.height = "0px";
+        }
+    };
+}
